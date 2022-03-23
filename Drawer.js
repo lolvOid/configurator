626c31569a04cbfe296398689fd110a371555341
lolvOid <linhtet1998itachi@gmail.com>
2022-03-24 04:19:40 +0630

    Updated

---
+++ Drawer.js
@@ -0,0 +1,74 @@
+
+
+function Drawer(mesh) {
+    this.geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.material = new THREE.MeshStandardMaterial({
+        color: 0xff7f50
+    });
+    this.mesh = mesh;
+    this.name = "Drawer";
+    this.map = null
+    
+
+    this.set_name = function (name) {
+        this.mesh.name = name;
+    }
+    this.set_color = function(color){
+        this.material.color.set = color;
+    }
+    this.set_texture = function(tex){
+    
+        this.material.map =  new THREE.TextureLoader().load(tex);
+    }
+    this.create = function () {
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.name = this.name;
+        scene.add(this.mesh);
+    }
+
+    this.remove = function () {
+        scene.remove(this.mesh);
+    }
+    this.setPosition = function (vector3) {
+        if (this.mesh instanceof THREE.Mesh) {
+            this.mesh.position = vector3;
+        }
+    }
+    this.setScale = function (vector3) {
+        if (this.mesh instanceof THREE.Mesh) {
+            this.mesh.scale = vector3;
+        }
+    }
+    this.set_position = function (pX, pY, pZ) {
+        if (this.mesh instanceof THREE.Mesh) {
+
+            this.mesh.position.setX(pX);
+            this.mesh.position.setY(pY);
+            this.mesh.position.setZ(pZ);
+
+        }
+
+    }
+    this.set_scale = function (sX, sY, sZ) {
+        if (this.mesh instanceof THREE.Mesh) {
+           
+            this.mesh.scale.setX(sX);
+            this.mesh.scale.setY(sY);
+            this.mesh.scale.setZ(sZ);
+
+        }
+    }
+
+    this.visible = function (bool) {
+        this.mesh.visible = bool;
+    }
+
+    this.getPosition = function(){
+       return this.mesh.position;
+        
+
+    }
+    this.getScale = function(){
+        return this.mesh.scale;
+    }
+}
